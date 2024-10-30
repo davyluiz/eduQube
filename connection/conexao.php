cec5d38c4c94574767abb0e6c9a49f57296f5bd1
Davy Oliveira <davyldsoliveira@hotmail.com>
2024-10-30 19:48:18 -0300

    Remove arquivos da estrutura

--- connection/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USUARIO', 'eduqube');
-define('SENHA', '&3eduq7kb3@');
-define('DB', 'eduqube');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
